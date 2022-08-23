b28bf9a14a351ffe0fbd2f78b2d13a41221ee159
mAniruddhac <97304641+mAniruddhac@users.noreply.github.com>
2022-08-23 21:34:55 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,6 @@
+package javaApp;
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello! you have successfully integrated maven with jenkins. Congratulations!!!");
+	}
+}
